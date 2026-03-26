d62145e00f8def9958faade6cf7c699bd8954c18
Annie Zheng <annie.zheng@manchester.ac.uk>
2026-03-26 16:06:05 +0000

    Add docstrings

--- toolkits/clients/validation_client.py
+++ toolkits/clients/validation_client.py
@@ -4,6 +4,8 @@ from urllib.parse import urljoin
 from toolkits.config.settings import CRATEY_VALIDATOR_API_URL
 
 def validate_rocrate_metadata(data):
+    """Send payload to the RO-Crate metatdata validation API and return the response."""
+
     url = urljoin(CRATEY_VALIDATOR_API_URL, "v1/ro_crates/validate_metadata")
     headers = {
         "accept": "application/json",
--- toolkits/services/validation_service.py
+++ toolkits/services/validation_service.py
@@ -4,6 +4,8 @@ from toolkits.clients.validation_client import validate_rocrate_metadata
 
 
 def is_rocrate_metadata_valid(data):
+    """Return True when the data is valid RO-Crate metadata"""
+
     result = validate_rocrate_metadata(data).get("result", "{}")
     try:
         result_json = json.loads(result)
