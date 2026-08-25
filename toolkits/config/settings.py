844b980d555998b2b11be518f8fb5dc93d6aa690
Annie Zheng <annie.zheng@manchester.ac.uk>
2026-08-25 16:13:19 +0100

    Remove unused env vars from settings

--- toolkits/config/settings.py
+++ toolkits/config/settings.py
@@ -11,8 +11,6 @@ def _get_required_env(name: str) -> str:
     return value
 
 CRATEY_VALIDATOR_API_URL = _get_required_env("CRATEY_VALIDATOR_API_URL")
-TES_SUBMISSION_API_URL = _get_required_env("TES_SUBMISSION_API_URL")
-TES_SUBMISSION_API_TOKEN = _get_required_env("TES_SUBMISSION_API_TOKEN")
 
 ROCRATE_METADATA_FILENAME = "ro-crate-metadata.json"
 
